31f9ebd805955e7a751283cda98f06dd9b8b50c8
Lemon <mishav97@gmail.com>
2022-09-29 20:25:46 +0500

    feat: add ip message and file parsers that return ParsedIPs instance

--- handlers/client.py
+++ handlers/client.py
@@ -3,6 +3,7 @@ from aiogram.types import ContentTypes, Message
 from create_bot import dp, bot
 from os import path
 from config import DOCUMENT_DIR
+from ip_parsers import ip_parser as ip_get
 
 
 async def process_start_command(message: types.Message):
@@ -13,25 +14,28 @@ async def process_help_command(message: types.Message):
     await message.reply(f"Send me something and i send it back in reverse")
 
 
-async def echo_message(msg: types.Message):
-    print(msg)
-    msg_backwards = msg.text[::-1]
-    await bot.send_message(msg.from_user.id, msg_backwards)
+async def parse_message(msg: types.Message):
+    parsed_ips = await ip_get.message_text_parse(msg.text)
+    print(parsed_ips)
+    await bot.send_message(msg.from_user.id, msg.text)
 
 
-async def check_if_file(msg: Message):
+async def parse_document(msg: Message):
     if document := msg.document:
-        print(document.file_name)
-        document_path = await document.download(
-            destination_dir=DOCUMENT_DIR
-        )
-        abs_path_for_document = document_path.name
-        print(abs_path_for_document)
-
+        file_name, file_extension = path.splitext(document.file_name)
+        if file_extension == '.txt':
+            document_path = await document.download(
+                destination_dir=DOCUMENT_DIR
+            )
+            abs_path_for_document = document_path.name
+            parsed_ips = await ip_get.file_text_parse(abs_path_for_document)
+            print(parsed_ips)
+        else:
+            await msg.reply(f"IP bot can only parse '.txt' files, please provide one")
 
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(process_start_command, commands=['start'])
     dp.register_message_handler(process_help_command, commands=['help'])
-    dp.register_message_handler(echo_message, content_types=ContentTypes.TEXT)
-    dp.register_message_handler(check_if_file, content_types=ContentTypes.DOCUMENT)
+    dp.register_message_handler(parse_message, content_types=ContentTypes.TEXT)
+    dp.register_message_handler(parse_document, content_types=ContentTypes.DOCUMENT)
--- ip_parsers/ip_parser.py
+++ ip_parsers/ip_parser.py
@@ -1,15 +1,47 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from enum import Enum
+from ipaddress import ip_address, IPv4Address, IPv6Address
+import re
 
 
-@dataclass
+@dataclass(slots=True, kw_only=True)
 class ParsedIPs:
-    ip_count: int
-    ip_lst: list
+    ip_count: int = field(init=False)
+    ip_lst: list[str] = field(default_factory=list)
+
+    def __post_init__(self) -> None:
+        self.ip_count = len(self.ip_lst)
 
 
-async def message_text_parse(msg) -> ParsedIPs:
-    pass
+class RegexParsePatterns(Enum):
+    ipv4 = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    ipv6 = r'\w{1,4}\:\w{1,4}\:\w{1,4}\:\w{1,4}\:\w{1,4}\:\w{1,4}\:\w{1,4}\:\w{1,4}\b'
 
 
-async def file_text_parse(msg) -> ParsedIPs:
-    pass
+async def message_text_parse(text: str) -> ParsedIPs:
+    validated_ip_lst = await text_parse(text)
+    return ParsedIPs(ip_lst=validated_ip_lst)
+
+
+async def file_text_parse(filepath: str) -> ParsedIPs:
+    with open(filepath, 'r', encoding='utf-8', errors='ignore') as file:
+        validated_ip_lst = await text_parse(file.read().replace("\n", ""))
+    return ParsedIPs(ip_lst=validated_ip_lst)
+
+
+async def text_parse(text: str) -> list:
+    validated_ip_lst = []
+    for ip_pattern in RegexParsePatterns:
+        possible_ip_lst = re.findall(ip_pattern.value, text)
+        for possible_ip in possible_ip_lst:
+            valid_ip = await _validate_ip(possible_ip)
+            if valid_ip:
+                validated_ip_lst.append(str(valid_ip))
+    return validated_ip_lst
+
+async def _validate_ip(ip: str) -> IPv4Address | IPv6Address | None:
+    try:
+        valid_ip = ip_address(ip)
+        return valid_ip
+    except ValueError:
+        return None
